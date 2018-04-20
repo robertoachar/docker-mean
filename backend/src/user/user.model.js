472078ae076afada2886dfe4524795e6024df35e
Roberto Achar <robertoachar@gmail.com>
2018-04-20 15:55:33 -0300

    refactor(backend): format using prettier

--- backend/src/user/user.model.js
+++ backend/src/user/user.model.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-var UserSchema = new Schema(
-  {
-    firstname: String,
-    lastname: String
-  });
+var UserSchema = new Schema({
+  firstname: String,
+  lastname: String
+});
 
 module.exports = mongoose.model('User', UserSchema);
